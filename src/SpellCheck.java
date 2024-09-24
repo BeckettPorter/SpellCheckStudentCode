fe7ff5548232cfdbb6966f713c42e6bfd5801fab
BeckettPorter <porterbeckett@gmail.com>
2024-09-24 09:25:09 -0700

    Made map and almost got it working.

--- src/SpellCheck.java
+++ src/SpellCheck.java
@@ -1,10 +1,17 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+
 /**
  * Spell Check
  * A puzzle written by Zach Blick
  * for Adventures in Algorithms
  * At Menlo School in Atherton, CA
  *
- * Completed by: [YOUR NAME HERE]
+ * Completed by: Beckett Porter
+ * Completed on:
  * */
 
 public class SpellCheck {
@@ -15,10 +22,39 @@ public class SpellCheck {
      *
      * @param text The list of all words in the text.
      * @param dictionary The list of all accepted words.
-     * @return String[] of all mispelled words in the order they appear in text. No duplicates.
+     * @return String[] of all misspelled words in the order they appear in text. No duplicates.
      */
-    public String[] checkWords(String[] text, String[] dictionary) {
+    public String[] checkWords(String[] text, String[] dictionary)
+    {
+        Map<String, Boolean> stringIntMap = new Hashtable<>();
+        Map<String, Boolean> misspelledWords = new Hashtable<>();
 
-        return null;
+        for (String word : dictionary)
+        {
+            stringIntMap.put(word, false);
+        }
+
+        for (int i = 0; i < text.length; i++)
+        {
+            if (!stringIntMap.containsKey(text[i]) && !misspelledWords.get(text[i]))
+            {
+                misspelledWords.put(text[i], true);
+            }
+        }
+
+
+        String[] ar = new String[misspelledWords.size()];
+
+        int index = 0;
+        for (String s : misspelledWords.keySet())
+        {
+            ar[index] = s;
+            index++;
+        }
+
+        return ar;
+
+        // Made hashmap of all dictionary words
+        // Check if word is in dictionary
     }
 }
